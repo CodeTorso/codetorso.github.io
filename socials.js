45df999ab165e7ee97a51094ae935b778cdfa737
CodeTorso <pruthvirajthinks@gmail.com>
2023-10-24 15:54:07 +0530

    add some more links

--- socials.js
+++ socials.js
@@ -1,21 +1,54 @@
-$(document).ready(function(){
-  animateDiv('.a');
-  animateDiv('.b');
-  animateDiv('.c');
-  animateDiv('.d');
-});
+// Keep track of div positions
+let divPositions = {}; 
 
-function makeNewPosition(){
-  var h = document.querySelector('.circle-space').offsetHeight;
-  var w = document.querySelector('.circle-space').offsetWidth;
-  var nh = Math.floor(Math.random() * h);
-  var nw = Math.floor(Math.random() * w);
-  return [nh, nw];    
+function makeNewPosition(div){
+
+  // Get container dimensions
+  let h = document.querySelector('.circle-space').offsetHeight; 
+  let w = document.querySelector('.circle-space').offsetWidth;
+
+  // Generate random x, y within container  
+  let newY = Math.floor(Math.random() * h);
+  let newX = Math.floor(Math.random() * w);
+
+  // Check if new position overlaps with existing divs
+  let overlaps = false;
+  for(let key in divPositions) {
+    if(Math.abs(newX - divPositions[key].x) < 50 &&  
+       Math.abs(newY - divPositions[key].y) < 50) {
+      overlaps = true;
+      break;
+    }
+  }
+
+  // If no overlap generate new position
+  if(!overlaps) {
+    divPositions[div] = {x: newX, y: newY}; 
+    return [newY, newX];
+  } else {
+    return makeNewPosition(div); // Recursively generate new position
+  }
+
 }
 
-function animateDiv(myclass){
-  var newq = makeNewPosition();
-  $(myclass).animate({ top: newq[0], left: newq[1] }, 5000,   function(){
-    animateDiv(myclass);        
+
+// Rest of code remains the same
+
+$(document).ready(function(){
+  animateDiv('.a');
+  animateDiv('.b');  
+  animateDiv('.c');
+  animateDiv('.d');
+  animateDiv('.e');
+  animateDiv('.f');
+});
+
+function animateDiv(div){
+
+  let newPos = makeNewPosition(div); // Pass div selector
+
+  $(div).animate({ top: newPos[0], left: newPos[1] }, 5000, function(){
+    animateDiv(div); 
   });
+
 };
